008871f86e94e5912d73888d87126a499698d54c
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-08-07 19:50:24 -0300

    Update Investigador.java

--- Investigador.java
+++ Investigador.java
@@ -1,61 +1,55 @@
 package Lista2;
 
-import java.beans.Encoder;
 import java.util.Scanner;
 
 public class Investigador {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-		String perg1, perg2, perg3, perg4, perg5;
-		int contador;
+		String perg1 = " ", perg2 = " ", perg3 = " ", perg4 = " ", perg5 = " ";
+		int contador = 0;
 		
 		System.out.println("==== Responda SIM ou NÃO sobre o crime ====");
-		System.out.println("Telefonou para a vítima? R: ");
-		perg1 = entrada.nextLine();
-		//entrada.nextLine();
-		System.out.println("Esteve no local do crime? R: ");
-		perg2 = entrada.nextLine();
-		//entrada.nextLine();
-		System.out.println("Mora perto da vítima? R: ");
-		perg3 = entrada.nextLine();
-		//entrada.hasNextLine();
-		System.out.println("Devia para vítima? R: ");
-		perg4 = entrada.nextLine();
-		//entrada.nextLine();
-		System.out.println("Já trabalhou com a vítima? R: ");
-		perg5 = entrada.nextLine();
-		
-		if (perg1 == "SIM" || perg1 == "sim") {
-			contador = 1;
-		} else {
-			contador = 0;
-		}
-		
-		if (perg2 == "SIM" || perg2 == "sim") {
+		System.out.print("Telefonou para a vítima? R: ");
+		perg1 = entrada.nextLine().toUpperCase();
+		if (perg1.equals("SIM")) {
 			contador = contador + 1;
-		} else {
-			contador = 0;
 		}
 		
-		if (perg3 == "SIM" || perg3 == "sim") {
+		System.out.print("Esteve no local do crime? R: ");
+		perg2 = entrada.nextLine().toUpperCase();
+		if (perg2.equals("SIM")) {
 			contador = contador + 1;
-		} else {
-			contador = 0;
 		}
 		
-		if (perg4 == "SIM" || perg4 == "sim") {
+		System.out.print("Mora perto da vítima? R: ");
+		perg3 = entrada.nextLine().toUpperCase();
+		if (perg3.equals("SIM")) {
 			contador = contador + 1;
-		} else {
-			contador = 0;
 		}
 		
-		if (perg5 == "SIM" || perg5 == "sim") {
+		System.out.print("Devia para vítima? R: ");
+		perg4 = entrada.nextLine().toUpperCase();
+		if (perg4.equals("SIM")) {
 			contador = contador + 1;
-		} else {
-			contador = 0;
 		}
 		
+		System.out.print("Já trabalhou com a vítima? R: ");
+		perg5 = entrada.nextLine().toUpperCase();
+		if (perg5.equals("SIM")) {
+			contador = contador + 1;
+		}
+		
+		/*if (contador == 1 || contador == 2) {
+			System.out.println("Status: Suspeito(a)");
+		} else if (contador == 3 || contador == 4) {
+			System.out.println("Status: Cúmplice");
+		} else if (contador == 5) {
+			System.out.println("Status: Assassino(a)");
+		} else {
+			System.out.println("Status: Inocente");
+		}*/
+				
 		switch (contador) {
 		case 1:
 		case 2:
